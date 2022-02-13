a247b0bcde2e23de49b55a7306942324b475d299
Roberto <rbrtarenhart@gmail.com>
2022-02-12 21:56:51 -0300

    Especific routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  # get '/inicio', to: 'welcome#index'
+
   resources :coins
+  # get '/coins', to: 'coins#index' #Especific route
 
   root to: 'welcome#index' #Default page, action index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
